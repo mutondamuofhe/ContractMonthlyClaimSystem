d74294446819419d776aa76438686257fbe38f6a
RC_Student_Lab <RC_Student_Lab@LabVM1846780>
2025-11-21 15:12:39 +0200

    FIXING ERRORS

--- ContractMonthlyClaimSystem_starter/Models/ApplicationUser.cs
+++ ContractMonthlyClaimSystem_starter/Models/ApplicationUser.cs
@@ -3,6 +3,6 @@
 public class ApplicationUser : IdentityUser
 {
     public string EmployeeId { get; set; }
-    public string RoleKind { get; set; } // Lecturer, ProgrammeCoordinator, AcademicManager
+    public string RoleKind { get; set; } // Lecturer, ProgrammeCoordinator, AcademicManager,HR
     public string Department { get; set; }
 }
